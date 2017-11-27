d62af29f3dd2079baf04c8202d5467305399a799
Stephan Volpers <svolpers@users.noreply.github.com>
2017-11-27 12:52:17 +0100

    Update line colors for more than 30 moderator values

--- R/plot.simpleslopes.R
+++ R/plot.simpleslopes.R
@@ -1,6 +1,9 @@
 #' @title plots simple slopes
 #' @description
-#' Opens a new graphical window and plots simple slopes
+#' Opens a new graphical window and plots simple slopes. 
+#' CAUTION: 
+#' No more than 30 different values of the moderator allowed. 
+#' I recommend a maximum of six to plot.
 #' @param x An object of type \code{simpleslopes} or 
 #' \code{simpleslopes.bootmi}
 #' @param ... any default arguments
@@ -10,14 +13,27 @@
 #' @export
 
 plot.simpleslopes <- function( x, ... ) {
+  
   # extract information from slopes object
   mod_vals = round( x$info$Values_of_Moderator, 2)
   xv = x$plot$x # values on x-axis
   yv = x$plot$y # values on y-axis
-  
+
+
+  if( length(mod_vals) > 30 ) {
+    stop("No more than 30 different moderator values allowed for plotting.")
+  }
+
   # create title and filename
   title = paste0( x$info$Y, " ~ ", x$info$X, " * ", x$info$M, "(+ ivs)")
   
+  
+  # extract position of starting points 
+  i = c( 1:( length( xv)))[c(T,F)]
+  # if more than six moderator values, use colors 
+  linecolors = rep( c("grey0","grey50","red","blue","cyan"), 
+      times= 1, length.out= NA, each= 6)
+
   # open new graphical window
   grDevices::dev.new(); graphics::par( mar= c(5,4,4,9))
   # plot start and end points of slopes
@@ -25,10 +41,9 @@ plot.simpleslopes <- function( x, ... ) {
     ylim= c((min(yv)),(max(yv))), xlim= c((min(xv)),(max(xv))), 
     bty= "L"
   )
-  # extract position of starting points 
-  i = c( 1:( length( xv)))[c(T,F)]
   # connect corresponding dots by lines
-  graphics::segments( xv[i], yv[i], xv[i+1], yv[i+1], lty=1:length(i))
+  graphics::segments( xv[i], yv[i], xv[i+1], yv[i+1], 
+    lty= 1:length(i), col= linecolors)
   # add legend
   graphics::legend( x= "topright", legend= mod_vals, inset= c(-.40,0), 
     lty= 1:length(i), cex= .8, ncol= 1, title= "Moderator Values", xpd= TRUE)

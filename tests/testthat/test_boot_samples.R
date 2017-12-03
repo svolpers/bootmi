8e5e46b5d4fae2f243bfed6d7f17a8ede6ee7790
Stephan Volpers <svolpers@users.noreply.github.com>
2017-12-03 14:45:47 +0100

    Updated data for test

--- tests/testthat/test_boot_samples.R
+++ tests/testthat/test_boot_samples.R
@@ -1,14 +1,13 @@
-
 context("Bootstrap samples")
+
+# create data
+data = as.data.frame( matrix( 40:79, nrow = 10))
 
 test_that("data is matrix or data.frame", {
 	expect_error( boot_samples( "a"), "Matrix or data.frame needed.")
 })
 
 test_that("number of samples equals R", {
-	# create data
-	data = as.data.frame( matrix( 1:12, nrow = 4))
-	# tests
 	expect_equal( length( boot_samples( data, -2)), 4)
 	expect_equal( length( boot_samples( data, 0)), 2)
 	expect_equal( length( boot_samples( data, 1)), 1)
@@ -16,9 +15,6 @@ test_that("number of samples equals R", {
 })
 
 test_that("sample size equals original data", {
-	# create data
-	data = as.data.frame( matrix( 40:79, nrow = 10))
-	# tests
 	expect_equal( nrow( boot_samples( data, 1)[[1]]), 10)
 	expect_equal( nrow( boot_samples( data[2:5,], 1)[[1]]), 4)
 })
